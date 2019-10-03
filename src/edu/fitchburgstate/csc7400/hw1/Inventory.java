36413fda661f08455e40206cd580bd4c94823cda
alolam <55292616+alolam@users.noreply.github.com>
2019-10-03 11:57:45 -0500

    Update Inventory.java

--- src/edu/fitchburgstate/csc7400/hw1/Inventory.java
+++ src/edu/fitchburgstate/csc7400/hw1/Inventory.java
@@ -1,41 +1,40 @@
-package edu.fitchburgstate.csc7400.hw1;
 /*
  * class:Object Oriented Analysis and Design
- *Instructor:Orlando Montalvo
- *Assignment:1
- *Student Name:Akhila Lolam
+ * Instructor:Orlando Montalvo
+ * Assignment:1
+ * Student Name: Akhila Lolam (@01408514)
  */
-
+package edu.fitchburgstate.csc7400.hw1;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+
 /**
-*Inventory contains all information about guitars that are available in store which will help the owner to help him match customers of their choice
+* Inventory contains all information about guitars that are available in store which will help the owner to help him match customers of their choice
 * and for customers to find suitable guitars.
-*
-* @author akhila
 *
 */
 
 public class Inventory {
   private List<Guitar> guitars;
-  /**
-     *constructor
-     *@param serialNumber
-     *@param price
-     *@param builder
-     *@param model
-     *@param type
-     *@param backwood
-     *@param topWood
-     */
-  /** List of guitars that are presently available */
+  
+  // Stores list of all guitars that are presently available 
   
   public Inventory() {
+   // creating object of linked list
     guitars = new LinkedList<Guitar>();
   }
   
-//to create a new guitar instance
+/**
+* constructor
+* @param serialNumber manufactures serial number
+* @param price stores price
+* @param builder the guitar builder
+* @param model the manufacture model
+* @param type guitar type
+* @param backWood the wood used for guitar body
+* @param topWood the wood used for guitar face
+*/
 
   public void addGuitar(String serialNumber, double price,
                         String builder, String model,
@@ -45,15 +44,15 @@ public class Inventory {
     guitars.add(guitar);
   }
   
-  /**
-* 
+/**
+* getGuitar takes guitar serialNumber
 * @return the guitar object
 *
 */
-   
-  
   public Guitar getGuitar(String serialNumber) {
+    //hasNext returns true if it has more guitar elements to iterate
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
+     //it returns the next element of the list
       Guitar guitar = (Guitar)i.next();
       if (guitar.getSerialNumber().equals(serialNumber)) {
         return guitar;
@@ -62,14 +61,20 @@ public class Inventory {
     return null;
   }
   /**
-	 * It would return the name of the manufacturer
-	  and store in the string builder
-	 */
+   * It would return the name of the manufacturer
+   * and store in the string builder
+   */
   public Guitar search(Guitar searchGuitar) {
+      //hasNext returns true if it has more guitar elements to iterate
     for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
+      //it returns the next element of the list
+      // if there is no next element in the list it throws exception
       Guitar guitar = (Guitar)i.next();
       // Ignore serial number since that's unique
       // Ignore price since that's unique
+      /* Returns the name of the manufacturer and stores in the
+      * string buffer
+      */
       String builder = searchGuitar.getManufacturer();
       if ((builder != null) && (!builder.equals("")) &&
           (!builder.equals(guitar.getManufacturer()))) //Without the case sensitivity it compares  manufacturer name
@@ -95,3 +100,4 @@ public class Inventory {
     return null;
   }
 }
+
