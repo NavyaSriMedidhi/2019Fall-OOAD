213526f8e0375b48bc8a26830d8433ecad5a3d35
kyathi <55417062+kyathi@users.noreply.github.com>
2019-10-03 10:37:14 -0400

    Update GuitarTest.java

--- test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
+++ test/edu/fitchburgstate/csc7400/hw1/GuitarTest.java
@@ -1,16 +1,37 @@
+
+/**
+* Course: Object-oriented analysis and design
+* Instructor: Orlando Montalvo
+* Assignment: HW1
+*Student Name:Kyathi
+* Date:03/10/2019
+*/
 package edu.fitchburgstate.csc7400.hw1;
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
-public class GuitarTest {
+/**
+* This tests the guitar class and throws the exception encountered
+*
+* @author Kyathi
+*
+*/
 
-	@Before
+public class GuitarTest {
+/**
+* Handles and Throws Exception
+*/	
+	
+         @Before
+	
 	public void setUp() throws Exception {
 	}
-
+ /**
+*tests the program
+*/
 	@Test
+        
 	public void test() {
 		fail("Not yet implemented");
 	}
